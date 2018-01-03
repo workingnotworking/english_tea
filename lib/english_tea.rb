f19718fe9e78b1688736cef5646881afb8287495
Rob Cameron <cannikinn@gmail.com>
2018-01-02 16:21:52 -0800

    ch8458 Only load Rails engine if included in rails, configure Sass load paths always

--- lib/english_tea.rb
+++ lib/english_tea.rb
@@ -1,5 +1,43 @@
-require "english_tea/rails/engine"
 require "english_tea/version"
 
 module EnglishTea
+
+  class << self
+
+    # inspired by boostrap-sass
+
+    def load!
+      register_rails_engine if rails?
+      configure_sass
+    end
+
+    def rails?
+      defined?(::Rails)
+    end
+
+    def gem_path
+      File.expand_path '..', File.dirname(__FILE__)
+    end
+
+    def assets_path
+      File.join(gem_path, 'app', 'assets')
+    end
+
+    def stylesheets_path
+      File.join(assets_path, 'stylesheets')
+    end
+
+    private def register_rails_engine
+      require 'english_tea/rails/engine'
+    end
+
+    private def configure_sass
+      require 'sass'
+      ::Sass.load_paths << stylesheets_path
+    end
+
+  end
+
 end
+
+EnglishTea.load!
